15d4bf7b879c92e5b03c1f8c6bd50bac47b16daa
Anshara-Ayaz-01 <ansharasiddiqui2003@gmail.com>
2025-08-05 07:10:05 +0500

    Fixing

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use('/api/tasks', taskRoutes);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => {
   console.log(' Connected to MongoDB');
